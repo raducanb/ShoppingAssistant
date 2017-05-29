df5867bad186c61dad69ceeb86912ba38486d058
Bogdan Raducan <bogdan.raducan@emag.ro>
2017-05-29 14:46:57 +0300

    Fix Application subclass for multidex support

--- android/app/src/main/java/com/example/raducanbogdan/shoppingassistant/MyApplication.java
+++ android/app/src/main/java/com/example/raducanbogdan/shoppingassistant/MyApplication.java
@@ -1,6 +1,7 @@
 package com.example.raducanbogdan.shoppingassistant;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import com.androidnetworking.AndroidNetworking;
 
@@ -8,7 +9,7 @@ import com.androidnetworking.AndroidNetworking;
  * Created by raducanb on 28/05/2017.
  */
 
-public class MyApplication extends Application {
+public class MyApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
